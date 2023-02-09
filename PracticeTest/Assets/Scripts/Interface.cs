8cb6e5952d84aa9190e0f5b12ba579c8ea31cc00
JUMBO\mandychu <mandychu@jumbogames.com.tw>
2023-02-09 14:57:53 +0800

    First Commit

---
+++ PracticeTest/Assets/Scripts/Interface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interface
+{
+    public interface I_Hurt
+    {
+        void Hurt();
+    }
+
+
+    public interface I_Attack
+    {
+        void Attack();
+    }
+
+
+    public interface I_Conversaction
+    {
+        void Conversaction();
+    }
+}
