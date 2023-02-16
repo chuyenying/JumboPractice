7581d1b6d4041929441292e55e587cb32bfd38a4
JUMBO\mandychu <mandychu@jumbogames.com.tw>
2023-02-16 16:11:06 +0800

    no message

---
+++ PracticeTest/Assets/Scripts/MVC/ButtonView.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonView : MonoBehaviour
+{
+    public delegate void TalkToController(int creature);
+    public event TalkToController talk_Controller;
+
+    public delegate void HurtToController(int creature);
+    public event TalkToController Hurt_Controller;
+
+    public delegate void AttackToController(int creature);
+    public event TalkToController Attack_Controller;
+
+    public delegate void ConversactionToController(int creature);
+    public event TalkToController Conversaction_Controller;
+
+    [Header("Player")]
+    [SerializeField] private Button Player_Talk;
+    [SerializeField] private Button Player_Hurt;
+    [SerializeField] private Button Player_Attack;
+    [SerializeField] private Button Player_Conversaction;
+
+    [Header("People")]
+    [SerializeField] private Button People_Talk;
+    [SerializeField] private Button People_Hurt;
+    [SerializeField] private Button People_Conversaction;
+
+    [Header("Monster")]
+    [SerializeField] private Button Monster_Talk;
+    [SerializeField] private Button Monster_Hurt;
+    [SerializeField] private Button Monster_Attack;
+
+
+    private void Start()
+    {
+
+        Player_Talk.onClick.AddListener(() => { Talk_OnClick(0); }); // 當按下玩家說話按鈕呼叫 OnClick func
+        Player_Hurt.onClick.AddListener(() => { Hurt_OnClick(0); });
+        Player_Attack.onClick.AddListener(() => { Attack_OnClick(0); });
+        Player_Conversaction.onClick.AddListener(() => { Conversaction_OnClick(0); });
+
+
+        People_Talk.onClick.AddListener(() => { Talk_OnClick(1); }); // 當按下玩家說話按鈕呼叫 OnClick func
+        People_Hurt.onClick.AddListener(() => { Hurt_OnClick(1); });
+        People_Conversaction.onClick.AddListener(() => { Conversaction_OnClick(1); });
+
+        Monster_Talk.onClick.AddListener(() => { Talk_OnClick(2); }); // 當按下玩家說話按鈕呼叫 OnClick func
+        Monster_Hurt.onClick.AddListener(() => { Hurt_OnClick(2); });
+        Monster_Attack.onClick.AddListener(() => { Attack_OnClick(2); });
+
+    }
+
+    public void Talk_OnClick(int creature)
+    {
+        talk_Controller(creature);  //觸發event
+    }
+    public void Hurt_OnClick(int creature)
+    {
+        Hurt_Controller(creature);
+    }
+    public void Attack_OnClick(int creature)
+    {
+        Attack_Controller(creature);
+    }
+    public void Conversaction_OnClick(int creature)
+    {
+        Conversaction_Controller(creature);
+    }
+
+
+    public void Output(string DebugLog_View)
+    {
+        Debug.Log(DebugLog_View);
+    }
+}
---
+++ PracticeTest/Assets/Scripts/MVC/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private ModelController modelController;
+    [SerializeField] private ButtonView buttonView;
+
+    private void Start()
+    {
+        buttonView.talk_Controller += Talk;
+        buttonView.Hurt_Controller += Hurt;
+        buttonView.Attack_Controller += Attack;
+        buttonView.Conversaction_Controller += Conversaction;
+    }
+
+    public void Talk(int creature)
+    {
+        buttonView.Output(modelController.Talk(creature));
+    }
+    public void Hurt(int creature)
+    {
+        buttonView.Output(modelController.Hurt(creature));
+    }
+    public void Attack(int creature)
+    {
+        buttonView.Output(modelController.Attack(creature));
+    }
+    public void Conversaction(int creature)
+    {
+        buttonView.Output(modelController.Conversaction(creature));
+    }
+}
