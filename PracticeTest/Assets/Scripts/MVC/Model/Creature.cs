20352e58ec7cefba0054330f7053f616a37e4d1c
JUMBO\mandychu <mandychu@jumbogames.com.tw>
2023-03-29 16:04:30 +0800

    no message

---
+++ PracticeTest/Assets/Scripts/MVC/Model/Creature.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Creature
+{
+    [Header("Creature基本資料")]
+    public string name;
+    public int hp;
+    public int attack;
+    public int creature;
+
+    [Header("Mode")]
+    public bool Talk;
+    public bool Hurt;
+
+    public bool Attack;
+    public bool Conversaction;
+    
+}
+
+
